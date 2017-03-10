429c0f2cd2c009bfbe271576b4d31d6c55606b37
manuc <manu@manucrea.com>
2017-03-10 02:24:25 -0600

    segunda ronda
    Avances sobre html, barra de navegación, aún nada sobre test.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+var elementPosition = $('#navigation').offset();
+
+$(window).scroll(function(){
+  if($(window).scrollTop() > elementPosition.top){
+    $('#navigation').addClass('fixed');
+  } else {
+    $('#navigation').removeClass('fixed');
+  }    
+});
